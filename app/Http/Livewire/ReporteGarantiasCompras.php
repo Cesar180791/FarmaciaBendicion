5f66e1e711a965ca5a57ac8f0750350076cffeea
Cesar180791 <mondragonrivera2013@gmail.com>
2022-03-31 14:41:05 -0600

    COmponentes Livewire reporte compras, reporte garantias sobre compras

---
+++ app/Http/Livewire/ReporteGarantiasCompras.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ReporteGarantiasCompras extends Component
+{
+    public function render()
+    {
+        return view('livewire.reporte-garantias-compras');
+    }
+}
